36f6d50e5a378c9933d0b0431447776521985f9b
fredokun <Frederic.Peschanski@lip6.fr>
2013-07-13 20:46:01 +0200

    env_ident for closing env

--- test/test_parser.py
+++ test/test_parser.py
@@ -41,7 +41,8 @@ contents of the command
     def test_env_simple(self):
         parser = ScripTexParser()
 
-        # BREAKPOINT >>> # import pdb; pdb.set_trace()  # <<< BREAKPOINT #
+        # BREAKPOINT >>> #
+        import pdb; pdb.set_trace()  # <<< BREAKPOINT #
         ret = parser.parse_from_string(r"""
 % a comment before the environment
 \begin{env}
