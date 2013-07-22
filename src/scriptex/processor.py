204e8a1a7d5072c8ba55be7def83da9702178466
Frederic Peschanski <Frederic.Peschanski.work@gmail.com>
2013-07-22 22:27:29 +0200

    section processor

--- src/scriptex/processor.py
+++ src/scriptex/processor.py
@@ -3,14 +3,17 @@ by interpreting commands and/or environments.
 
 """
 
+from scriptex.markup import Markup
+
 class ProcessError(Exception):
     pass
 
-class DocumentProcessing:
+class DocumentProcessor:
     def __init__(self, document):
         self.document = document
         self.cmd_processors = dict()
         self.env_processors = dict()
+        self.sec_processors = dict()
         
     def register_command_processor(self, cmd_name, cmd_processor):
         if cmd_name in self.cmd_processors:
@@ -24,11 +27,24 @@ class DocumentProcessing:
 
         self.env_processors[env_name] = env_processor
 
+    def register_section_processor(self, sec_depth, sec_processor):
+        if sec_depth in self.sec_processors:
+            raise ProcessError("Processor for section level {}: already registered".format(sec_depth))
+
+        if not self.sec_processors:
+            self.sec_processors[0] = sec_processor # global processor if only one
+        else:
+            del self.sec_processors[0] # no global processor if more than one
+            
+        self.sec_processors[sec_depth] = sec_processor
+
+        
     def process(self):
         # Stack[Markup * Int]   (doc/cmd/env, index in child, -1 for unprocessed)
-        self.markup_stack = [(document, 0, None, -1)]
+        self.markup_stack = [(self.document, 0, None, -1)]
         self.environment_stack = []
         self.command_stack = []
+        self.section_stack = []
 
         while self.markup_stack:
             self.markup, self.content_index, self.source_markup, self.source_index = self.markup_stack.pop()
@@ -41,6 +57,12 @@ class DocumentProcessing:
                     if self.markup.env_name in self.env_processors:
                         self.env_processors[self.markup.env_name].enter_environment(self, self.markup)
                     self.environment_stack.append(self.markup)
+                elif self.markup.markup_type == "section":
+                    if 0 in self.sec_processors:
+                        self.sec_processors[0].enter_section(self, self.markup)
+                    elif self.markup.section_depth in self.sec_processors:
+                        self.sec_processors[self.markup.section_depth].enter_secton(self, self.markup)
+                    self.section_stack.append(self.markup)
                 # push back in queue but next time process content at index 0 (first child)
                 self.markup_stack.append((self.markup, 0, self.source_markup, self.source_index))
             else: # processing of markup already started
@@ -64,6 +86,19 @@ class DocumentProcessing:
                                 if recursive:
                                     self.markup_stack.append((new_content, -1, self.source_markup, self.source_index))
                                 self.source_markup.content[self.source_index] = new_content
+                    elif self.markup.markup_type == "section":
+                        check_sec = self.section_stack.pop()
+                        assert check_sec == self.markup, "Invalid section stack (please report)"
+                        new_content = None
+                        recursive = False
+                        if 0 in self.sec_processors:
+                            new_content, recursive = self.sec_processors[0].process_section(self, self.markup)
+                        elif self.markup.section_depth in self.sec_processors:
+                            new_content, recursive = self.sec_processors[self.markup.section_depth].process_section(self, self.markup)
+                        if new_content is not None:
+                            if recursive:
+                                self.markup_stack.append((new_content, -1, self.source_markup, self.source_index))
+                            self.source_markup.content[self.source_index] = new_content
                 else: # process a child
                     child = self.markup.content[self.content_index]
                     self.markup_stack.append((self.markup, self.content_index+1, self.source_markup, self.source_index))
@@ -98,3 +133,12 @@ class EnvironmentProcessor:
         return (None, False)  # default process is :  do nothing
 
 
+class SectionProcessor:
+    def __init__(self):
+        pass
+
+    def enter_section(self, processing, cmd):
+        pass # should be overriden
+
+    def process_section(self, processing, cmd):
+        return (None, False)  # default process is :  do nothing
---
+++ test/test_processor.py
@@ -0,0 +1,39 @@
+'''
+Test processor
+'''
+
+import unittest
+
+if __name__ == "__main__":
+    import sys
+    sys.path.append("../src")
+
+from scriptex.parser import Parser
+from scriptex.processor import DocumentProcessor, CommandProcessor, EnvironmentProcessor
+
+class TestCommandProcess(unittest.TestCase):
+
+    class CountItem(CommandProcessor):
+        def __init__(self):
+            self.count = 0
+
+        def enter_command(self, processing, cmd):
+            self.count += 1
+
+    def test_count_item(self):
+        parser = Parser()
+        # BREAKPOINT >>> # import pdb; pdb.set_trace()  # <<< BREAKPOINT #
+        doc = parser.parse_from_file("../examples/basic.scrip.tex")
+
+        processor = DocumentProcessor(doc)
+
+        count = TestCommandProcess.CountItem()
+        processor.register_command_processor("item",count)
+
+        processor.process()
+
+        #print("count = {}".format(count.count))
+        self.assertEqual(count.count, 5)
+        
+if __name__ == '__main__':
+    unittest.main()
