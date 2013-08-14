8011198aee5073d8301695855e2ca21b14060ae4
Frederic Peschanski <Frederic.Peschanski@lip6.fr>
2013-08-14 12:19:52 +0200

    defPython added

--- src/scriptex/processors/codeactive.py
+++ src/scriptex/processors/codeactive.py
@@ -13,6 +13,7 @@ class PythonContext:
     def __init__(self):
         self.globals = dict()  # dictionnary of globals
         self.pprint = pprint.PrettyPrinter()
+        self.defs = dict()
 
     def eval_python_expr(self, expr, filename='<unknown>', line_pos=None):
         code = ast.parse(expr, filename, 'eval')
@@ -32,6 +33,10 @@ class PythonContext:
             
         exec(ccode, self.globals)
 
+    def def_python(self, def_name, def_source, filename='<unknown>', line_pos=None):
+        self.exec_python(def_source, filename, line_pos)
+        # TODO: check the def in the context
+        self.defs[def_name] = def_source # TODO: register only if defined        
 
 class EvalPythonProcessor(CommandProcessor):
     def __init__(self, python_context):
@@ -44,6 +49,18 @@ class EvalPythonProcessor(CommandProcessor):
         output = self.python_context.pprint.pformat(ret)
         return (markup.Preformated(output, "python-3", cmd.start_pos, cmd.end_pos), False)
 
+class DefPythonProcessor(CommandProcessor):
+    def __init__(self, python_context):
+        super().__init__()
+        self.python_context = python_context
+
+    def process_command(self, processor, cmd):
+        # BREAKPOINT >>> # import pdb; pdb.set_trace()  # <<< BREAKPOINT #
+        self.python_context.def_python(cmd.cmd_opts, cmd.content, processor.document.filename, cmd.header_end_pos.lpos)
+        # TODO: log the registered function
+        return (markup.SkipMarkup(cmd.start_pos, cmd.end_pos), False)
+        
+    
 def register_processors(processor, python_context):
     processor.register_command_processor("evalPython", EvalPythonProcessor(python_context))
-
+    processor.register_command_processor("defPython", DefPythonProcessor(python_context))
--- test/test_exos.py
+++ test/test_exos.py
@@ -13,14 +13,32 @@ from scriptex.processor import DocumentProcessor
 from scriptex.processors import core, codeactive
 from scriptex.generators.latex.article import LatexArticleGenerator
 
-
-
 class TestExos(unittest.TestCase):
 
     def test_eval_python(self):
         parser = Parser()
         doc = parser.parse_from_string(""""
 \evalPython{{{3+2}}}
+""")
+
+        #print("Before processing = \n" + str(doc));
+
+        process = DocumentProcessor(doc)
+        py_ctx = codeactive.PythonContext()
+        codeactive.register_processors(process, py_ctx)
+        process.process()
+
+        #print("After processing = \n" + str(doc));
+
+    def test_def_python(self):
+        parser = Parser()
+        doc = parser.parse_from_string(""""
+\defPython[fact]{{{
+def fact(n):
+    return 1 if n<=1 else n*fact(n-1)
+}}}
+
+\evalPython{{{3+fact(4)}}}
 """)
 
         print("Before processing = \n" + str(doc));
@@ -31,6 +49,7 @@ class TestExos(unittest.TestCase):
         process.process()
 
         print("After processing = \n" + str(doc));
+        
     
     def test_exos_liste(self):
         # 1) parsing
