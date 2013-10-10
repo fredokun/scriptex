088ac0a44f2000e1761b1aa0accb01ef3c02b936
Frederic Peschanski <Frederic.Peschanski@lip6.fr>
2013-10-10 11:57:57 +0200

    alternative escape variable tested

--- test/test_template.py
+++ test/test_template.py
@@ -20,6 +20,14 @@ class TestBasicTemplates(unittest.TestCase):
         ret = template.render(locals())
         print(ret)
 
+    def test_alt_variable_escape(self):
+        template = Template("The value of myvar is '?myvar' obtained by ??myvar\n and that's it ...", escape_var='?')
+        template.compile()
+        # print(template.ctemplate)
+        myvar = 42
+        ret = template.render(locals())
+        print(ret)
+
 if __name__ == '__main__':
     unittest.main()
 
