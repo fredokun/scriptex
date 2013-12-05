32c448dbc6e5a627fa9cc7b2f08463a55e9aa40a
Frederic Peschanski <frederic.peschanski@lip6.fr>
2013-12-05 15:40:29 +0100

    partial integration of subdoc (non-working)

---
+++ test/test_subdoc.py
@@ -0,0 +1,39 @@
+'''
+Test sub documents
+'''
+
+import unittest
+
+if __name__ == "__main__":
+    import sys
+    sys.path.append("../src")
+
+from tangolib.parser import Parser
+from tangolib.processor import DocumentProcessor, CommandProcessor, EnvironmentProcessor
+
+class TestSubDoc(unittest.TestCase):
+
+    class CountItem(CommandProcessor):
+        def __init__(self):
+            self.count = 0
+
+        def enter_command(self, processing, cmd):
+            self.count += 1
+
+    def test_count_item(self):
+        parser = Parser()
+        # BREAKPOINT >>> # import pdb; pdb.set_trace()  # <<< BREAKPOINT #
+        doc = parser.parse_from_file("../examples/basic.tango.tex")
+
+        processor = DocumentProcessor(doc)
+
+        count = TestCommandProcess.CountItem()
+        processor.register_command_processor("item",count)
+
+        processor.process()
+
+        #print("count = {}".format(count.count))
+        self.assertEqual(count.count, 5)
+        
+if __name__ == '__main__':
+    unittest.main()
