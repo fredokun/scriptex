d3623415b2d1f08d735b1de94dcf206ab13ddf6b
Frederic Peschanski <frederic.peschanski@lip6.fr>
2013-12-10 10:12:17 +0100

    cleaning compiled python

--- clean.sh
+++ clean.sh
@@ -6,3 +6,6 @@ find ./ -name "*~" -exec rm -f {} \;
 echo "Removing python cache"
 find ./ -name "__pycache__" -exec rm -rf {} \;
 
+echo "Removing compiled python files"
+find ./ -name "*.pyc" -exec rm -f {} \;
+
